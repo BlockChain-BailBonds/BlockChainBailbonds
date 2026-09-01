43995dbe48f26cd6c08bc7de0eb2518dee1c2b01
Nine1Eight <founder918tech@gmail.com>
2026-09-01 17:27:32 -0500

    Remove obsolete protocol test for deleted non-production firmware contract

--- m3rma1d-s1r3n/tests/protocol_test.cpp
+++
@@ -1,4 +0,0 @@
-#include <cassert>
-#include "protocol.hpp"
-using namespace s1r3n;
-int main(){assert(capabilityAllowed(Capability::DeviceInfo));assert(capabilityAllowed(Capability::IrTransmit));assert(needsApproval(Capability::IrTransmit));assert(!needsApproval(Capability::DeviceInfo));const char*s="123456789";assert(crc32((const uint8_t*)s,9)==0xCBF43926u);JobRequest j{};j.job_id=42;j.capability=Capability::DeviceInfo;j.timeout_ms=500;assert(sizeof(j)<128);return 0;}
