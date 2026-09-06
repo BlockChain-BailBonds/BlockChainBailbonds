bcb873221479a42a66460f06a753122599795f63
Nine1Eight <founder918tech@gmail.com>
2026-09-06 06:07:33 -0500

    Add typed MermaidLink UART protocol for Flipper

---
+++ m3rma1d-s1r3n/firmware/flipper-m3rma1d-s1r3n/mermaid_link.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <furi.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MERMAID_LINK_VERSION 2U
+#define MERMAID_LINK_BAUD 230400U
+#define MERMAID_LINK_MAX_PAYLOAD 96U
+
+typedef enum {
+    MermaidMsgHello = 1,
+    MermaidMsgStatusRequest = 2,
+    MermaidMsgStatus = 3,
+    MermaidMsgCatalogRequest = 4,
+    MermaidMsgCatalog = 5,
+    MermaidMsgActionRequest = 6,
+    MermaidMsgActionResult = 7,
+    MermaidMsgStop = 8,
+    MermaidMsgApproval = 9,
+} MermaidMsgType;
+
+typedef struct {
+    bool codex_linked;
+    bool s3_linked;
+    bool camera_ready;
+    bool stop_asserted;
+    bool approval_pending;
+    uint32_t heartbeat_ms;
+    uint32_t packets_rx;
+    uint32_t packets_tx;
+    uint32_t faults;
+} MermaidRemoteStatus;
+
+typedef struct {
+    uint16_t total;
+    uint16_t ready;
+    uint16_t needs_adapter;
+    uint16_t blocked;
+} MermaidCatalogSummary;
+
+typedef struct MermaidLink MermaidLink;
+
+typedef void (*MermaidStatusCallback)(const MermaidRemoteStatus* status, void* context);
+typedef void (*MermaidCatalogCallback)(const MermaidCatalogSummary* catalog, void* context);
+typedef void (*MermaidResultCallback)(uint32_t job_id, int16_t code, const char* text, void* context);
+
+MermaidLink* mermaid_link_alloc(void);
+void mermaid_link_free(MermaidLink* link);
+bool mermaid_link_is_open(const MermaidLink* link);
+void mermaid_link_set_callbacks(
+    MermaidLink* link,
+    MermaidStatusCallback status_cb,
+    MermaidCatalogCallback catalog_cb,
+    MermaidResultCallback result_cb,
+    void* context);
+void mermaid_link_poll(MermaidLink* link);
+bool mermaid_link_request_status(MermaidLink* link);
+bool mermaid_link_request_catalog(MermaidLink* link);
+bool mermaid_link_send_stop(MermaidLink* link, uint32_t job_id);
+
+#ifdef __cplusplus
+}
+#endif
